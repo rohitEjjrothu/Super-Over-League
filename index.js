e15d38f58d3999a632e59d43bc4eedd9704153be
rohitEjjrothu <evenkatsairohit@gmail.com>
2024-03-14 09:37:34 +0000

    Authentication

---
+++ index.js
@@ -0,0 +1,12 @@
+const element = (
+  <div className='bg-cointainer'>
+   <h1 className="heading">Super Over League</h1>
+   <div className="images-cointainer">
+      <img className="images"sec="https://assets.ccbp.in/frontend/react-js/rcb-img.png">
+      <img className="images"sec="https://assets.ccbp.in/frontend/react-js/csk-img.png">
+   </div>
+  </div>
+  //   Write your code here.
+)
+
+ReactDOM.render(element, document.getElementById('root'))
